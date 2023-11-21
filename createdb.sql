3298f186c0217187ce0d80bf3b8c11427897238c
gana <63591509+gantumurbattumur@users.noreply.github.com>
2023-11-21 10:42:09 -0800

    Add files via upload

---
+++ createdb.sql
@@ -0,0 +1,31 @@
+create table `user` (
+`Account_id` INT
+, `Username` varchar(50)
+, `Saved_Data` int 
+);
+insert into `user` values(
+
+)
+create table `missions`(
+`Mission_id` int 
+, `Name` varchar(255)
+, `Level_id` int 
+)
+CREATE TABLE  `levels`(
+`Difficulty` varchar(50)
+, `Mission_id` int
+)
+create table `skins` (
+`Skin_id` int
+, `Mission_id` int
+, `Ch_id` int
+
+)
+create table `character`(
+`Ch_id` int
+, `Ch_name` varchar(255)
+, `Skills` varchar(155)
+, `Range` varchar(155)
+, `Skins_id` int
+
+)

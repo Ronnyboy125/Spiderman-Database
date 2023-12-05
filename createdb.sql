c073e6d89a84855deb32ba7b44ff8e9be48929c0
Ron Schwencer <119630244+Ronnyboy125@users.noreply.github.com>
2023-12-05 10:07:06 -0800

    test createdb.sql

--- createdb.sql
+++ createdb.sql
@@ -3,6 +3,56 @@ create table `user` (
 , `Username` varchar(50)
 , `Saved_Data` int 
 );
+insert into `user` values (
+  1
+  ,'Gana'
+  ,5
+  );
+insert into `user` values (
+  2
+  ,'Ron'
+  ,8
+  );
+insert into `user` values (
+  3
+  ,'Pete'
+  ,10
+  );
+insert into `user` values (
+  4
+  ,'Scott'
+  ,2
+  );
+insert into `user` values (
+  5
+  ,'Kent'
+  ,4
+  );
+insert into `user` values (
+  6
+  ,'Bell'
+  ,8
+  );
+insert into `user` values (
+  7
+  ,'Qian'
+  ,7
+  );
+insert into `user` values (
+  8
+  ,'Matthew'
+  ,3
+  );
+insert into `user` values (
+  9
+  ,'Nolan'
+  ,1
+  );
+insert into `user` values (
+  10
+  ,'Spencer'
+  ,0
+  );
 create table `missions`(
 `Mission_id` int 
 , `Name` varchar(255)
